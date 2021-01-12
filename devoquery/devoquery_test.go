bb11b7a7d7474cc52a82f5fbb433d3bf92b81797
Luis David Barrios <cyberluisda@gmail.com>
2021-01-12 13:22:49 +0100

    [devoquery]  Unit tests for 'NewTokenEngineDefaultQuery' implemented

--- devoquery/devoquery_test.go
+++ devoquery/devoquery_test.go
@@ -69,3 +69,55 @@ func TestNewTokenEngine(t *testing.T) {
 		})
 	}
 }
+
+func TestNewTokenEngineDefaultQuery(t *testing.T) {
+	// Values
+	query := "from test.keep.free"
+	type args struct {
+		apiURL string
+		token  string
+		query  string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *QueryEngineToken
+		wantErr bool
+	}{
+		{
+			"Error: Empty query",
+			args{
+				apiURL: "http://apiurl.does.not.exits.org",
+				token:  "token",
+			},
+			nil,
+			true,
+		},
+		{
+			"Query engine created",
+			args{
+				apiURL: "http://apiurl.does.not.exits.org",
+				token:  "token",
+				query:  query,
+			},
+			&QueryEngineToken{
+				apiURL:       "http://apiurl.does.not.exits.org",
+				token:        "token",
+				DefaultQuery: &query,
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewTokenEngineDefaultQuery(tt.args.apiURL, tt.args.token, tt.args.query)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewTokenEngineDefaultQuery() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewTokenEngineDefaultQuery() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
