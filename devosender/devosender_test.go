aa96a581f5eb56d4d93eeb1426300c7e7f9f9aa7
Luis David Barrios <cyberluisda@gmail.com>
2021-01-15 16:22:46 +0100

    [devosender] Unit tests for SendWTagAsync fixed

--- devosender/devosender_test.go
+++ devosender/devosender_test.go
@@ -277,30 +277,22 @@ func TestClient_WaitForPendingAsyngMessages(t *testing.T) {
 }
 
 func TestClient_SendWTagAsync(t *testing.T) {
-	type fields struct {
-		entryPoint        string
-		syslogHostname    string
-		defaultTag        string
-		conn              net.Conn
-		ReplaceSequences  map[string]string
-		tls               *tlsSetup
-		waitGroup         sync.WaitGroup
-		asyncErrors       map[string]error
-		asyncErrorsMutext sync.Mutex
-	}
 	type args struct {
 		t string
 		m string
 	}
 	tests := []struct {
 		name   string
-		fields fields
+		client *Client
 		args   args
 		want   *regexp.Regexp
 	}{
 		{
 			"Expected id pattern",
-			fields{},
+			func() *Client {
+				sender, _ := NewDevoSender("udp://example.org:80") // real public service which we can stablish udp connection
+				return sender
+			}(),
 			args{
 				t: "tag",
 				m: "message",
@@ -310,17 +302,7 @@ func TestClient_SendWTagAsync(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			dsc := &Client{
-				entryPoint:        tt.fields.entryPoint,
-				syslogHostname:    tt.fields.syslogHostname,
-				defaultTag:        tt.fields.defaultTag,
-				conn:              tt.fields.conn,
-				ReplaceSequences:  tt.fields.ReplaceSequences,
-				tls:               tt.fields.tls,
-				waitGroup:         tt.fields.waitGroup,
-				asyncErrors:       tt.fields.asyncErrors,
-				asyncErrorsMutext: tt.fields.asyncErrorsMutext,
-			}
+			dsc := tt.client
 			if got := dsc.SendWTagAsync(tt.args.t, tt.args.m); !tt.want.Match([]byte(got)) {
 				t.Errorf("Client.SendWTagAsync() = %v, want matching with %v", got, tt.want.String())
 			}
