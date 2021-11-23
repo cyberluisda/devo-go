1f7f161680347f5d72ed36d1da1f4b0aaf8a72de
Luis David Barrios <cyberluisda@gmail.com>
2021-11-23 19:09:36 +0100

    [devosender] Unit tests for ReliableClientBuilder.EnableStandByModeTi...

--- devosender/devosender_reliable_test.go
+++ devosender/devosender_reliable_test.go
@@ -194,6 +194,62 @@ func TestReliableClientBuilder_DaemonStopTimeout(t *testing.T) {
 	}
 }
 
+func TestReliableClientBuilder_EnableStandByModeTimeout(t *testing.T) {
+	type fields struct {
+		enableStandByModeTimeout time.Duration
+	}
+	type args struct {
+		d time.Duration
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *ReliableClientBuilder
+	}{
+		{
+			"Duration eq to 0",
+			fields{
+				time.Hour,
+			},
+			args{0 * time.Millisecond},
+			&ReliableClientBuilder{
+				enableStandByModeTimeout: 0,
+			},
+		},
+		{
+			"Duration less to 0",
+			fields{
+				time.Hour,
+			},
+			args{-1 * time.Second},
+			&ReliableClientBuilder{
+				enableStandByModeTimeout: time.Second * -1,
+			},
+		},
+		{
+			"Duration greater than 0",
+			fields{
+				0,
+			},
+			args{time.Minute},
+			&ReliableClientBuilder{
+				enableStandByModeTimeout: time.Minute,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dsrcb := &ReliableClientBuilder{
+				enableStandByModeTimeout: tt.fields.enableStandByModeTimeout,
+			}
+			if got := dsrcb.EnableStandByModeTimeout(tt.args.d); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ReliableClientBuilder.EnableStandByModeTimeout() = %+v, want %+v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestReliableClient_String(t *testing.T) {
 	type fields struct {
 		Client                   *Client
