83e5954c58fdeb32416247c83a86a3a7868ae57f
Luis David Barrios <cyberluisda@gmail.com>
2021-12-01 10:11:10 +0100

    [devosender] LazyClient struct will be "SwitchDevoSender" implementation
    In fact it will be on-memory implementation of `SwitchDevoSender` interface

--- devosender/devosender_lazy.go
+++ devosender/devosender_lazy.go
@@ -2,6 +2,7 @@ package devosender
 
 import (
 	"io"
+	"sync"
 	"time"
 
 	"github.com/cyberluisda/devo-go/applogger"
@@ -35,4 +36,32 @@ type LazyClientBuilder struct {
 	enableStandByModeTimeout time.Duration
 	flushTimeout             time.Duration
 	appLogger                applogger.SimpleAppLogger
+}
+
+// LazyClient is a SwitchDevoSender that save events in a buffer when it is in "stand by" mode.
+// Events are saved in a circular buffer, and when limit of buffer size is reached, new arrived
+// events are saved at the begining of the buffer.
+// when WakeUp is called, all events in buffer as send using Client.Async funcs in starting for first
+// element in slide. This implies that arrived event order can not be maintained if buffer suffered
+// an "oversize" situation.
+type LazyClient struct {
+	*Client
+	clientBuilder *ClientBuilder
+	bufferSize    uint32
+	flushTimeout  time.Duration
+	buffer        []lazyClientRecord
+	appLogger     applogger.SimpleAppLogger
+	clientMtx     sync.Mutex
+}
+
+// lazyClientRecord is the internal structure to save in memory of the events while
+// client is in standby mode
+// and allow operations like resend.
+type lazyClientRecord struct {
+	AsyncID    string
+	Timestamp  time.Time
+	Tag        string
+	Msg        string
+	Compressor *Compressor
+	LastError  error
 }
