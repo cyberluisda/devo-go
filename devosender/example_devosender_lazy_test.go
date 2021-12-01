b965b8aa914361840b23665c7e47d567ceba1a45
Luis David Barrios <cyberluisda@gmail.com>
2021-12-01 16:18:52 +0100

    [devosender] Example test about use LazyClient on stand-by mode

--- devosender/example_devosender_lazy_test.go
+++ devosender/example_devosender_lazy_test.go
@@ -2,6 +2,7 @@ package devosender
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -64,4 +65,64 @@ func ExampleNewLazyClientBuilder() {
 	// Output:
 	// 1: error Undefined inner client builder - LazyClient <nil>
 	// 2: error <nil> - LazyClient bufferSize: 256000, standByMode: false, #eventsInBuffer: 0, flushTimeout: 2s, Client: {entryPoint: 'udp://localhost:13000', ...
+}
+
+func ExampleLazyClient_StandBy() {
+
+	lc, err := NewLazyClientBuilder().
+		ClientBuilder(
+				NewClientBuilder().EntryPoint("udp://localhost:13000")). // udp protocol never return error
+		BufferSize(2). // very small buffers of two events only
+		Build()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("LazyClient", lc.String()[:117])
+
+	// Pass to stand by mode
+	err = lc.StandBy()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("IsStandBy", lc.IsStandBy())
+
+	// send messages in stand by mode
+	err = lc.SendWTag("test.keep.free", "message 1") // Should return error because client is in standby mode
+	fmt.Println("SendWTag error", err)
+
+	id := lc.SendWTagAsync("test.keep.free", "message 2")
+	fmt.Println("ID has non-conn- prefix", strings.HasPrefix(id, "non-conn-"))
+
+	fmt.Println("Stats", lc.Stats)
+
+	lc.SendWTagAsync("test.keep.free", "message 3")
+	lc.SendWTagAsync("test.keep.free", "message 4")
+	fmt.Println("Stats", lc.Stats)
+	fmt.Println("LazyClient", lc)
+
+	err = lc.WakeUp()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Stats after WakeUp", lc.Stats)
+	fmt.Println("LazyClient after WakeUp", lc.String()[0:117])
+
+	var sender SwitchDevoSender
+	sender = lc
+	sender.Close()
+	fmt.Println("LazyClient as SwitchDevoSender closed", sender.String())
+
+	// Output:
+	// LazyClient bufferSize: 2, standByMode: false, #eventsInBuffer: 0, flushTimeout: 2s, Client: {entryPoint: 'udp://localhost:13000'
+	// IsStandBy true
+	// SendWTag error Receiver func call with nil pointer
+	// ID has non-conn- prefix true
+	// Stats AsyncEvents: 1, TotalBuffered: 1, BufferedLost: 0, SendFromBuffer: 0
+	// Stats AsyncEvents: 3, TotalBuffered: 3, BufferedLost: 1, SendFromBuffer: 0
+	// LazyClient bufferSize: 2, standByMode: true, #eventsInBuffer: 2, flushTimeout: 2s, Client: {<nil>}
+	// Stats after WakeUp AsyncEvents: 3, TotalBuffered: 3, BufferedLost: 1, SendFromBuffer: 2
+	// LazyClient after WakeUp bufferSize: 2, standByMode: false, #eventsInBuffer: 0, flushTimeout: 2s, Client: {entryPoint: 'udp://localhost:13000'
+	// LazyClient as SwitchDevoSender closed bufferSize: 2, standByMode: true, #eventsInBuffer: 0, flushTimeout: 2s, Client: {<nil>}
 }
