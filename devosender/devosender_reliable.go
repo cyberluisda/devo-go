41735aa34ad9300200e2f2bc798c5cb6baff9e8a
Luis David Barrios <cyberluisda@gmail.com>
2021-12-27 16:03:44 +0100

    [devosender] status db dead-lock fixed in ReliableClient.dbInitCleanup

--- devosender/devosender_reliable.go
+++ devosender/devosender_reliable.go
@@ -877,9 +877,8 @@ func (dsrc *ReliableClient) dbInitCleanup() error {
 	}
 
 	dsrc.dbInitCleanedup = true // Assuming db is cleaned by default
+	dsrc.dbMtx.Lock()           // Lock status database
 
-	dsrc.dbMtx.Lock()
-	defer dsrc.dbMtx.Unlock()
 	err := dsrc.db.Update(func(tx *nutsdb.Tx) error {
 		// Check if we have elements using count
 		c, err := cont(tx, statsBucket, countKey, false)
@@ -954,6 +953,8 @@ func (dsrc *ReliableClient) dbInitCleanup() error {
 
 		return err
 	})
+
+	dsrc.dbMtx.Unlock() // We should unlock status database, because ConsolidateStatusDb will lock by itself
 
 	if err != nil {
 		dsrc.dbInitCleanedup = false
