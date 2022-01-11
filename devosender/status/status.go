0372f6fbfc1c36951a1da09a25703460c5b040ac
Luis David Barrios <cyberluisda@gmail.com>
2022-01-11 13:01:57 +0100

    [devosender/status] Initial version of 'cont' func
    This func is used to set the value of a key in a specific bucket of nutsdb. Value must be any kind of integer.

--- devosender/status/status.go
+++ devosender/status/status.go
@@ -217,6 +217,28 @@ func cont(tx *nutsdb.Tx, bucket string, key []byte, errorIfNotFound bool) (int, 
 	return strconv.Atoi(string(ve.Value))
 }
 
+// set sets the integer value of a key. Value is transformed using strvconv.Atoi(string(value)).
+// This method should be used when you need inc or dec one value more than one time in
+// same transacion. For example
+// Use:
+// n,_ := cont(tx, "bucket", []byte("key"))
+// ...
+// n++
+// n++
+// ...
+// n--
+// set(tx, "bucket", []byte("key"), n)
+// Instead of:
+// n,_ := cont(tx, "bucket", []byte("key"))
+// ...
+// inc(tx, "bucket", []byte("key"), 1, false)
+// inc(tx, "bucket", []byte("key"), 1, false)
+// ...
+// dec(tx, "bucket", []byte("key"), 1, false)
+func set(tx *nutsdb.Tx, bucket string, key []byte, v int) error {
+	vStr := strconv.Itoa(v)
+	return tx.Put(bucket, key, []byte(vStr), 0)
+}
 
 // del remove a key. Usefull alias of tx.Delete when you are working with inc, dec, cont and set
 func del(tx *nutsdb.Tx, bucket string, key []byte) error {
