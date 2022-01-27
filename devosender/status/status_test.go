6db7e6745b81b844a658f648c9015af2f07d009e
Luis David Barrios <cyberluisda@gmail.com>
2022-01-27 13:34:56 +0100

    [devosender/status] Unit SorteableStringTime.Swap test reordered

--- devosender/status/status_test.go
+++ devosender/status/status_test.go
@@ -15,84 +15,6 @@ import (
 	"github.com/xujiajun/nutsdb"
 )
 
-func TestSorteableStringTime_Swap(t *testing.T) {
-	type fields struct {
-		Values     []string
-		Timestamps []time.Time
-	}
-	type args struct {
-		i int
-		j int
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   *SorteableStringTime
-	}{
-		{
-			"Swap elements minor first",
-			fields{
-				[]string{"b", "a"},
-				func() []time.Time {
-					r := make([]time.Time, 2)
-					var t time.Time
-					r[0] = t.Add(time.Second * 2)
-					r[1] = t.Add(time.Second)
-					return r
-				}(),
-			},
-			args{0, 1},
-			&SorteableStringTime{
-				Values: []string{"a", "b"},
-				Timestamps: func() []time.Time {
-					r := make([]time.Time, 2)
-					var t time.Time
-					r[0] = t.Add(time.Second)
-					r[1] = t.Add(time.Second * 2)
-					return r
-				}(),
-			},
-		},
-		{
-			"Swap elements major first",
-			fields{
-				[]string{"b", "a"},
-				func() []time.Time {
-					r := make([]time.Time, 2)
-					var t time.Time
-					r[0] = t.Add(time.Second * 2)
-					r[1] = t.Add(time.Second)
-					return r
-				}(),
-			},
-			args{1, 0},
-			&SorteableStringTime{
-				Values: []string{"a", "b"},
-				Timestamps: func() []time.Time {
-					r := make([]time.Time, 2)
-					var t time.Time
-					r[0] = t.Add(time.Second)
-					r[1] = t.Add(time.Second * 2)
-					return r
-				}(),
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			sst := &SorteableStringTime{
-				Values:     tt.fields.Values,
-				Timestamps: tt.fields.Timestamps,
-			}
-			sst.Swap(tt.args.i, tt.args.j)
-			if !reflect.DeepEqual(sst, tt.want) {
-				t.Errorf("SorteableStringTime.Swap() = %+v, want %+v", sst, tt.want)
-			}
-		})
-	}
-}
-
 func Test_recreateIdxInTx(t *testing.T) {
 	type setup struct {
 		initVals []*EventRecord
@@ -1518,4 +1440,82 @@ func toolTestExistKey(db *nutsdb.DB, bucket string, key []byte) bool {
 func toolTestDestroyDb(path string, db *nutsdb.DB) {
 	db.Close()
 	os.RemoveAll(path)
+}
+
+func TestSorteableStringTime_Swap(t *testing.T) {
+	type fields struct {
+		Values     []string
+		Timestamps []time.Time
+	}
+	type args struct {
+		i int
+		j int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *SorteableStringTime
+	}{
+		{
+			"Swap elements minor first",
+			fields{
+				[]string{"b", "a"},
+				func() []time.Time {
+					r := make([]time.Time, 2)
+					var t time.Time
+					r[0] = t.Add(time.Second * 2)
+					r[1] = t.Add(time.Second)
+					return r
+				}(),
+			},
+			args{0, 1},
+			&SorteableStringTime{
+				Values: []string{"a", "b"},
+				Timestamps: func() []time.Time {
+					r := make([]time.Time, 2)
+					var t time.Time
+					r[0] = t.Add(time.Second)
+					r[1] = t.Add(time.Second * 2)
+					return r
+				}(),
+			},
+		},
+		{
+			"Swap elements major first",
+			fields{
+				[]string{"b", "a"},
+				func() []time.Time {
+					r := make([]time.Time, 2)
+					var t time.Time
+					r[0] = t.Add(time.Second * 2)
+					r[1] = t.Add(time.Second)
+					return r
+				}(),
+			},
+			args{1, 0},
+			&SorteableStringTime{
+				Values: []string{"a", "b"},
+				Timestamps: func() []time.Time {
+					r := make([]time.Time, 2)
+					var t time.Time
+					r[0] = t.Add(time.Second)
+					r[1] = t.Add(time.Second * 2)
+					return r
+				}(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sst := &SorteableStringTime{
+				Values:     tt.fields.Values,
+				Timestamps: tt.fields.Timestamps,
+			}
+			sst.Swap(tt.args.i, tt.args.j)
+			if !reflect.DeepEqual(sst, tt.want) {
+				t.Errorf("SorteableStringTime.Swap() = %+v, want %+v", sst, tt.want)
+			}
+		})
+	}
 }
