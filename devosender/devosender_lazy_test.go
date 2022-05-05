0fc518b6b5a274dc4f671d8451a2780a323e4be2
Luis David Barrios <cyberluisda@gmail.com>
2022-05-05 14:07:14 +0200

    [devosender] Timeout decreased on case of LazyClient.StandBy  test

--- devosender/devosender_lazy_test.go
+++ devosender/devosender_lazy_test.go
@@ -22,7 +22,7 @@ func TestLazyClient_StandBy(t *testing.T) {
 			func() *LazyClient {
 				r, err := NewLazyClientBuilder().
 					ClientBuilder(NewClientBuilder().EntryPoint("tcp://example.com:80")). // We need a real connection
-					EnableStandByModeTimeout(time.Microsecond).                           // To force timeout
+					EnableStandByModeTimeout(time.Nanosecond).                            // To force timeout
 					Build()
 				if err != nil {
 					panic(err)
